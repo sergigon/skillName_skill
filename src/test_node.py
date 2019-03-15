aa194dc0dd870b2bc9873e6bacf52201978665ca
sergigon <39344908+sergigon@users.noreply.github.com>
2019-03-15 12:22:52 +0100

    Soft adaptation to new DMS

--- src/test_node.py
+++ src/test_node.py
@@ -10,145 +10,163 @@ __maintainer__ = "Elena Velázquez and Sergio González"
 __email__ = "serigon@ing.uc3m.es"
 __status__ = "Development"
 
-#from functions.dms_functions import my_import
+# Skill libraries
 from skill.skill import Skill, ActionlibException, CONDITIONAL
-from std_msgs.msg import String, Empty
-from std_srvs.srv import Empty
-from etts_msgs.msg import Utterance
-from perception_msgs.msg import PerceptionMessage
-from interaction_msgs.msg import CA
-from common_msgs.msg import KeyValuePair
-import key_value_pairs as kvpa
-
-import roslib
 import rospy
+import rospkg
 import actionlib
-import multimedia_msgs.msg ############# Cambiar esto #############
 
-pkg_name = 'test_skill' ############# Cambiar esto #############
-roslib.load_manifest(pkg_name)
+# Messages
+from std_msgs.msg import String, Empty
+import skillName_msgs.msg ############# Cambiar esto #############
 
+pkg_name = 'skillName_skill' ############# Cambiar esto #############
+
+# Skill variables
 # declare this only if the name is different of 'pkg_name'
-skill_name = "test_skill" ############# Cambiar esto #############
+skill_name = "skillName_skill" ############# Cambiar esto #############
 
-### rospy.loginfo("Subscriber: " + msg.data + str(self.__counter))
+class SkillNameSkill(Skill): ############# Cambiar esto #############
 
-class TestSkill(Skill): ############# Cambiar esto #############
-
-	_feedback = multimedia_msgs.msg.TestFeedback() ############# Cambiar esto #############
-	_result = multimedia_msgs.msg.TestResult() ############# Cambiar esto #############
+	_feedback = skillName_msgs.msg.SkillNameFeedback() ############# Cambiar esto #############
+	_result = skillName_msgs.msg.SkillNameResult() ############# Cambiar esto #############
 
 	def __init__(self):
 		"""
 		Init method.
 		"""
-		# init the skill
-		Skill.__init__(self, skill_name, CONDITIONAL)
 
 		# class variables
 		self._as = None
-		self._counter = 0
 		self._out = False
+		
+		# init the skill
+		Skill.__init__(self, skill_name, CONDITIONAL)
+	
+	def pause_exec(self):
+		"""
+		Callback called when a pause is requested.
+		"""
+
+		pass
+
+	def resume_exec(self):
+		"""
+		Callback called when a resume is requested.
+		"""
+
+		pass
 
 	def create_msg_srv(self):
 		"""
-		This function has to be implemented in the children.
+		Callback called when skill is started.
  		"""
- 		print("create_msg_srv")
+ 		rospy.loginfo("Start requested")
+		
 		# publishers and subscribers
+		self.ca_pub = rospy.Publisher(
+		    "hri_manager/ca_activations", CA, queue_size=1) # CA publisher
+		self.ca_deactivation_pub = rospy.Publisher(
+		    "hri_manager/ca_deactivations", String, queue_size=1) # CA deactivation publisher
 		# servers and clients
 
 		# actions
 		if not self._as:
 
-			self._as = actionlib.SimpleActionServer(pkg_name, multimedia_msgs.msg.TestAction, execute_cb=self.execute_cb, auto_start=False) ############# Cambiar esto #############
-
+			self._as = actionlib.SimpleActionServer(pkg_name, skillName_msgs.msg.SkillNameAction, execute_cb=self.execute_cb, auto_start=False) ############# Cambiar esto #############
 			# start the action server
 			self._as.start()
 
 	def shutdown_msg_srv(self):
 		"""
-		This function has to be implemented in the children.
+		Callback called when skill is stopped.
 		"""
 
 		# publishers and subscribers
 		# FIXME: do not unregister publishers because a bug in ROS
 		# self.__test_pub.unregister()
-
+		
+		rospy.loginfo("Stop requested")
 		# servers and clients
 
 
 	def execute_cb(self, goal): # Se activa cuando le envias un goal a la skill
 		"""
-		Spinner of the node.
+		Callback of the node. Activated when a goal is received
+
+        	@param goal: skillName_skill goal.
 		"""
-		rospy.loginfo('[TEST SKILL]') ############# Cambiar esto #############
+		# Init skill variables
+		self._out = False
 	
-		# default values (SUCCESS)
-		self._result.result = False
-		self._feedback.feedback = 0
-		
-		# Cojo informacion del goal recibido
-		################################################
-		if  goal.command != '':
-			rospy.loginfo("goal:" + str(goal.command))
-		else:
-			rospy.loginfo("goal vacio") 
-
-		while not self._out:
-			# sleep 2 seconds
-			rospy.sleep(2.0)
+		# Init result and feedback
+		# -- Result default values -- #
+		self._result.skill_result = self._result.SUCCESS # Success
+		# -- Feedback default values -- #
+		self._feedback.app_status = 'start_ok'
+		self._feedback.percentage_completed = 0
+		self._feedback.engagement = True
 			
-			print("self._status: "+ str(self._status) + ", self.RUNNING:" + str(self.RUNNING))
-			
-			############### Si la skill esta activa: ###################
-			if self._status == self.RUNNING:
-				print ("Running...")
-				try:
-					self._counter = self._counter + 1
-					print("self._counter: " + str(self._counter))
-					if self._counter >= 20:
-						self._result.result = True
-						self._out = True
+		####################### Skill active #######################
+		if self._status == self.RUNNING:
+			print ("Running...")
+			###################### Exec loop #######################
+			while not self._out:
+			try:
+				############# State Preempted checking #############
+				# If goal is in Preempted state (that is, there    #
+				# is a goal in the queue or the actual goal is     #
+				# cancelled), the exception is activated.          #
+				####################################################
+				if self._as.is_preempt_requested():
+					print("Preempt requested")
+					raise ActionlibException
+				#==================================================#
+				
+				##################### Process goal #####################
+            			rospy.loginfo('Goal: %s' % goal)
+				self._counter += 1
+				print("self._counter: " + str(self._counter))
+				if self._counter >= 20:
+					self._result.result = True
+					self._out = True
 
-					if self._as.is_preempt_requested():
-						print("Preempt requested")
-						raise ActionlibException
-
+				#==================================================#
+	
+				#################### Exceptions ####################
+                		### Preempted or cancel:
 				except ActionlibException:
-					
 					rospy.logwarn('[%s] Preempted or cancelled' % pkg_name)                 
 					# FAIL
-					self._result.result = 1
-					self._feedback.feedback = 0
-					
-			############################################################
-					
-			############# Si la skill no esta activa: ##################
-			else:
-				rospy.logwarn("[%s] Cannot send a goal when the skill is stopped" % pkg_name)
-				# ERROR
-				self._result.result = -1
-				self._feedback.feedback = 0
-			############################################################
+					self._result.skill_result = self._result.FAIL # Fail
+					self._feedback.app_status = 'cancel_ok'
+					self._out = True # Salgo del loop
+				#=================== Exceptions ===================#
+			#===================== Exec loop ======================#
 
+		#==================== Skill active ========================#
 
-			self._feedback.feedback = self._counter
-			self._as.publish_feedback(self._feedback)
-
-		print("Fuera del while")
-
-		if self._result.result:
-			self._as.set_succeeded(self._result)
+		##################### Skill NOT active #####################
 		else:
-			rospy.logdebug("setting goal to preempted")
-			self._as.set_preempted(self._result)
+			rospy.logwarn("[%s] Cannot send a goal when the skill is stopped" % pkg_name)
+			# ERROR
+			self._result.skill_result = self._result.FAIL # Fail
+		#==========================================================#
 
-
-		#Inicializacion variables
-		self._counter = 0
-		self._out = False
-
+		#### Result and feedback sending and goal status update ####
+		if self._result.skill_result == self._result.SUCCESS:
+		    rospy.logdebug("setting goal to succeeded")
+		    self._feedback.app_status = 'completed_ok'
+		    self._as.publish_feedback(self._feedback)
+		    self._as.set_succeeded(self._result)
+		else:
+		    rospy.logdebug("setting goal to preempted")
+		    self._feedback.app_status = 'completed_fail'
+		    self._as.publish_feedback(self._feedback)
+		    self._as.set_preempted(self._result)
+		rospy.loginfo("#############################")
+		rospy.loginfo("######## Result sent ########")
+		rospy.loginfo("#############################")
 
 
 if __name__ == '__main__':
@@ -156,23 +174,15 @@ if __name__ == '__main__':
 	try:
 		# start the node
 		rospy.init_node(skill_name)
+		rospy.loginfo('[' + pkg_name + ': ' + skill_name + ']')
 
 		# create and spin the node
-		node = TestSkill() ############# Cambiar esto #############
-		rospy.spin()
+		node = SkillNameSkill() ############# Cambiar esto #############
+		rospy.sleep(1)
+        
+		rate = rospy.Rate(10)
+		while not rospy.is_shutdown():
+		    rate.sleep()
 
 	except rospy.ROSInterruptException:
 		pass
-
-
-		'''
-		# Akinator
-			self._feedback.progression = self.progression
-			self._as.publish_feedback(self._feedback)
-
-		if self._result.result:
-			self._as.set_succeeded(self._result)
-		else:
-			rospy.logdebug("setting goal to preempted")
-			self._as.set_preempted(self._result)
-			'''
